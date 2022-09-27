c0b89e38603b3b8422e9e916c9d650c290510c94
Kimani <elvis.kimani@student.moringaschool.com>
2022-09-27 19:04:15 +0300

    commit tree 55ce6a9fe8c015d085a6e4e7db38c1be6914ed16

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
 
+  wrap_parameters format: []
+
   # GET /birds
   def index
     birds = Bird.all
@@ -20,6 +22,19 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  #post
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
